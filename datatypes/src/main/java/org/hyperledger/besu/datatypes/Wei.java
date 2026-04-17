8009532e167d017a6c2ad33cf97b8c867318fdfd
ahamlat <ameziane.hamlat@consensys.net>
2026-04-17 09:08:52 +0200

    Update datatypes/src/main/java/org/hyperledger/besu/datatypes/Wei.java
    Co-authored-by: Simon Dudley <simon.dudley@consensys.net>
    Signed-off-by: ahamlat <ameziane.hamlat@consensys.net>

--- datatypes/src/main/java/org/hyperledger/besu/datatypes/Wei.java
+++ datatypes/src/main/java/org/hyperledger/besu/datatypes/Wei.java
@@ -139,7 +139,7 @@ public final class Wei extends BaseUInt256Value<Wei> implements Quantity {
    * @param off the offset into the array where u3 should be written
    */
   public void writeLimbs(final long[] target, final int off) {
-    // TODO store this representation at build time when switching from v1 to v2
+    // TODO EVMv2 store this representation at Wei object construction time when switching from v2 to v1
     final byte[] b = toArrayUnsafe();
     target[off] = (long) LONG_BE.get(b, 0);
     target[off + 1] = (long) LONG_BE.get(b, 8);
