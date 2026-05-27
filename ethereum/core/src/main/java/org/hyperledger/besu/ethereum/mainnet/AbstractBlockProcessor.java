18ba5f2c431f62ccb95d67ab1dd468d483ce2822
Sagar Khandagre <sagar.khandagre998@gmail.com>
2026-05-27 10:33:00 +0530

    logging: demote StateRootMismatchException ERROR→INFO per maintainer guidance (#10160)
    Signed-off-by: Sagar Khandagre <sagar.khandagre998@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/AbstractBlockProcessor.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/AbstractBlockProcessor.java
@@ -556,7 +556,7 @@ public abstract class AbstractBlockProcessor implements BlockProcessor {
         RuntimeException rethrown = e;
         throw rethrown;
       } catch (StateRootMismatchException ex) {
-        LOG.error(
+        LOG.info(
             "failed persisting block due to stateroot mismatch; expected {}, actual {}",
             ex.getExpectedRoot().getBytes().toHexString(),
             ex.getActualRoot().getBytes().toHexString());
