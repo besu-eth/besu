a0ab775b51f03e9ef706249ffe5a07e5d1b39bbc
Luis Pinto <luis.pinto@consensys.net>
2026-07-10 10:51:40 +0100

    Add missing benchmark cases in ADDMOD v1 and minor fixes
    Signed-off-by: Luis Pinto <luis.pinto@consensys.net>

--- evm/src/main/java/org/hyperledger/besu/evm/v2/operation/AddModOperationV2.java
+++ evm/src/main/java/org/hyperledger/besu/evm/v2/operation/AddModOperationV2.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright contributors to Besu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.hyperledger.besu.evm.v2.operation;
 
 import org.hyperledger.besu.evm.UInt256;
 import org.hyperledger.besu.evm.frame.MessageFrame;
 import org.hyperledger.besu.evm.gascalculator.GasCalculator;
 
+/** The Add mod operation. */
 public class AddModOperationV2 extends AbstractFixedCostOperationV2 {
   private static final OperationResult addModSuccess = new OperationResult(8, null);
 
@@ -36,11 +51,11 @@ public class AddModOperationV2 extends AbstractFixedCostOperationV2 {
 
     final long[] stack = frame.stackDataV2();
     final UInt256 valueA =
-      new UInt256(stack[aOffset], stack[aOffset + 1], stack[aOffset + 2], stack[aOffset + 3]);
+        new UInt256(stack[aOffset], stack[aOffset + 1], stack[aOffset + 2], stack[aOffset + 3]);
     final UInt256 valueB =
-      new UInt256(stack[bOffset], stack[bOffset + 1], stack[bOffset + 2], stack[bOffset + 3]);
+        new UInt256(stack[bOffset], stack[bOffset + 1], stack[bOffset + 2], stack[bOffset + 3]);
     final UInt256 modulus =
-      new UInt256(stack[mOffset], stack[mOffset + 1], stack[mOffset + 2], stack[mOffset + 3]);
+        new UInt256(stack[mOffset], stack[mOffset + 1], stack[mOffset + 2], stack[mOffset + 3]);
 
     final UInt256 r = valueA.addMod(valueB, modulus);
 
