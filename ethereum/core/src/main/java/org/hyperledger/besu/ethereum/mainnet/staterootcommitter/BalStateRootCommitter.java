e6521079bc2bf2fde6478a59874e5b1973b1680a
Karim Taam <karim.t2am@gmail.com>
2026-07-20 14:20:53 +0400

    executor for storage
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/staterootcommitter/BalStateRootCommitter.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/staterootcommitter/BalStateRootCommitter.java
@@ -24,6 +24,7 @@ import org.hyperledger.besu.ethereum.ProtocolContext;
 import org.hyperledger.besu.ethereum.core.MutableWorldState;
 import org.hyperledger.besu.ethereum.mainnet.block.access.list.BlockAccessList;
 import org.hyperledger.besu.ethereum.mainnet.block.access.list.BlockAccessListAddressView;
+import org.hyperledger.besu.ethereum.mainnet.parallelization.BlockProcessingExecutors;
 import org.hyperledger.besu.ethereum.rlp.RLP;
 import org.hyperledger.besu.ethereum.trie.MerkleTrie;
 import org.hyperledger.besu.ethereum.trie.common.PmtStateTrieAccountValue;
@@ -218,7 +219,8 @@ public final class BalStateRootCommitter implements StateRootCommitter {
           storageFutures.put(
               address,
               CompletableFuture.supplyAsync(
-                  () -> updateStorageTrie(address, accountHash, blockAccessListAddressView)));
+                  () -> updateStorageTrie(address, accountHash, blockAccessListAddressView),
+                  BlockProcessingExecutors.ioExecutor()));
         }
       }
 
