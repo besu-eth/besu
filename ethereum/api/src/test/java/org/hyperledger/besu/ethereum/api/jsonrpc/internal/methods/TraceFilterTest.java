d4a0af620f52679eae390a8facee33df4dfe5eab
Ivan Bondarev <vania.bondarev5@gmail.com>
2026-08-03 00:38:29 +0300

    Test pre-execution across trace filter blocks
    Signed-off-by: Ivan Bondarev <vania.bondarev5@gmail.com>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/TraceFilterTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/TraceFilterTest.java
@@ -15,8 +15,15 @@
 package org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.JsonRpcRequest;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.JsonRpcRequestContext;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.parameters.BlockParameter;
@@ -24,17 +31,35 @@ import org.hyperledger.besu.ethereum.api.jsonrpc.internal.parameters.FilterParam
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.processor.BlockTracer;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcErrorResponse;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcResponse;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcSuccessResponse;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.RpcErrorType;
 import org.hyperledger.besu.ethereum.api.query.BlockchainQueries;
+import org.hyperledger.besu.ethereum.chain.Blockchain;
+import org.hyperledger.besu.ethereum.core.Block;
+import org.hyperledger.besu.ethereum.core.BlockDataGenerator;
+import org.hyperledger.besu.ethereum.eth.manager.EthScheduler;
+import org.hyperledger.besu.ethereum.mainnet.BlockProcessor;
+import org.hyperledger.besu.ethereum.mainnet.MiningBeneficiaryCalculator;
 import org.hyperledger.besu.ethereum.mainnet.ProtocolSchedule;
+import org.hyperledger.besu.ethereum.mainnet.ProtocolSpec;
+import org.hyperledger.besu.ethereum.mainnet.blockhash.PreExecutionProcessor;
+import org.hyperledger.besu.ethereum.mainnet.systemcall.BlockProcessingContext;
+import org.hyperledger.besu.evm.blockhash.BlockHashLookup;
+import org.hyperledger.besu.evm.tracing.OperationTracer;
 import org.hyperledger.besu.metrics.noop.NoOpMetricsSystem;
+import org.hyperledger.besu.plugin.services.worldstate.MutableWorldState;
 import org.hyperledger.besu.testutil.DeterministicEthScheduler;
 
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -46,6 +71,100 @@ public class TraceFilterTest {
   @Mock Supplier<BlockTracer> blockTracerSupplier;
   @Mock ProtocolSchedule protocolSchedule;
   @Mock BlockchainQueries blockchainQueries;
+
+  @Test
+  public void shouldApplyPreExecutionForEveryBlockInRange() throws InterruptedException {
+    final BlockDataGenerator blockGenerator = new BlockDataGenerator();
+    final Block genesis = blockGenerator.genesisBlock();
+    final Block firstBlock =
+        blockGenerator.block(
+            new BlockDataGenerator.BlockOptions()
+                .setBlockNumber(1)
+                .setParentHash(genesis.getHash())
+                .hasTransactions(false)
+                .hasOmmers(false));
+    final Block secondBlock =
+        blockGenerator.block(
+            new BlockDataGenerator.BlockOptions()
+                .setBlockNumber(2)
+                .setParentHash(firstBlock.getHash())
+                .hasTransactions(false)
+                .hasOmmers(false));
+    final Blockchain blockchain = mock(Blockchain.class);
+    final ProtocolSpec firstProtocolSpec = mock(ProtocolSpec.class);
+    final ProtocolSpec secondProtocolSpec = mock(ProtocolSpec.class);
+    final PreExecutionProcessor firstPreExecutionProcessor = mock(PreExecutionProcessor.class);
+    final PreExecutionProcessor secondPreExecutionProcessor = mock(PreExecutionProcessor.class);
+    final BlockHashLookup firstBlockHashLookup = mock(BlockHashLookup.class);
+    final BlockHashLookup secondBlockHashLookup = mock(BlockHashLookup.class);
+    final MutableWorldState firstWorldState = mock(MutableWorldState.class);
+    final MutableWorldState secondWorldState = mock(MutableWorldState.class);
+
+    when(blockchainQueries.headBlockNumber()).thenReturn(2L);
+    when(blockchainQueries.getBlockchain()).thenReturn(blockchain);
+    when(blockchain.getBlockByNumber(1L)).thenReturn(Optional.of(firstBlock));
+    when(blockchain.getBlockByNumber(2L)).thenReturn(Optional.of(secondBlock));
+    setupProtocolSpec(
+        blockchain,
+        firstBlock,
+        firstProtocolSpec,
+        firstPreExecutionProcessor,
+        firstBlockHashLookup);
+    setupProtocolSpec(
+        blockchain,
+        secondBlock,
+        secondProtocolSpec,
+        secondPreExecutionProcessor,
+        secondBlockHashLookup);
+
+    doAnswer(
+            invocation -> {
+              final Function<MutableWorldState, ?> mapper = invocation.getArgument(1);
+              return mapper.apply(
+                  invocation.getArgument(0).equals(firstBlock.getHeader().getParentHash())
+                      ? firstWorldState
+                      : secondWorldState);
+            })
+        .when(blockchainQueries)
+        .getAndMapWorldState(any(), any());
+
+    final NoOpMetricsSystem metricsSystem = new NoOpMetricsSystem();
+    final EthScheduler ethScheduler = new EthScheduler(1, 1, 1, metricsSystem);
+    method =
+        new TraceFilter(protocolSchedule, blockchainQueries, 100L, metricsSystem, ethScheduler);
+    final FilterParameter filterParameter =
+        new FilterParameter(
+            new BlockParameter(1L), new BlockParameter(2L), null, null, null, null, null, 1, 1);
+    final JsonRpcRequestContext request =
+        new JsonRpcRequestContext(
+            new JsonRpcRequest("2.0", "trace_filter", new Object[] {filterParameter}));
+
+    try {
+      final JsonRpcResponse response = method.response(request);
+
+      assertThat(response).isInstanceOf(JsonRpcSuccessResponse.class);
+      assertThat((ArrayNode) ((JsonRpcSuccessResponse) response).getResult()).hasSize(1);
+      assertPreExecutionContext(
+          firstPreExecutionProcessor,
+          firstBlock,
+          firstProtocolSpec,
+          firstBlockHashLookup,
+          firstWorldState);
+      assertPreExecutionContext(
+          secondPreExecutionProcessor,
+          secondBlock,
+          secondProtocolSpec,
+          secondBlockHashLookup,
+          secondWorldState);
+      verify(blockchainQueries)
+          .getAndMapWorldState(eq(firstBlock.getHeader().getParentHash()), any());
+      verify(blockchainQueries)
+          .getAndMapWorldState(eq(secondBlock.getHeader().getParentHash()), any());
+    } finally {
+      ethScheduler.stop();
+      ethScheduler.awaitStop();
+    }
+  }
 
   @ParameterizedTest
   @CsvSource({"0, 1001, 1000", "1, 6002, 1000", "1000, 3000, 500"})
@@ -83,5 +202,45 @@ public class TraceFilterTest {
 
     final JsonRpcErrorResponse errorResponse = (JsonRpcErrorResponse) response;
     assertThat(errorResponse.getErrorType()).isEqualTo(RpcErrorType.EXCEEDS_RPC_MAX_BLOCK_RANGE);
+  }
+
+  private void setupProtocolSpec(
+      final Blockchain blockchain,
+      final Block block,
+      final ProtocolSpec protocolSpec,
+      final PreExecutionProcessor preExecutionProcessor,
+      final BlockHashLookup blockHashLookup) {
+    final BlockProcessor blockProcessor = mock(BlockProcessor.class);
+    final MiningBeneficiaryCalculator miningBeneficiaryCalculator =
+        mock(MiningBeneficiaryCalculator.class);
+    when(blockchainQueries.getProtocolSpec(block.getHeader())).thenReturn(protocolSpec);
+    when(protocolSchedule.getByBlockHeader(block.getHeader())).thenReturn(protocolSpec);
+    when(protocolSpec.getPreExecutionProcessor()).thenReturn(preExecutionProcessor);
+    when(preExecutionProcessor.createBlockHashLookup(blockchain, block.getHeader()))
+        .thenReturn(blockHashLookup);
+    when(protocolSpec.getBlockReward()).thenReturn(Wei.ZERO);
+    when(protocolSpec.getBlockProcessor()).thenReturn(blockProcessor);
+    when(blockProcessor.getCoinbaseReward(Wei.ZERO, block.getHeader().getNumber(), 0))
+        .thenReturn(Wei.ZERO);
+    when(protocolSpec.getMiningBeneficiaryCalculator()).thenReturn(miningBeneficiaryCalculator);
+    when(miningBeneficiaryCalculator.calculateBeneficiary(block.getHeader()))
+        .thenReturn(Address.ZERO);
+  }
+
+  private void assertPreExecutionContext(
+      final PreExecutionProcessor preExecutionProcessor,
+      final Block block,
+      final ProtocolSpec protocolSpec,
+      final BlockHashLookup blockHashLookup,
+      final MutableWorldState worldState) {
+    final ArgumentCaptor<BlockProcessingContext> contextCaptor =
+        ArgumentCaptor.forClass(BlockProcessingContext.class);
+    verify(preExecutionProcessor).process(contextCaptor.capture(), eq(Optional.empty()));
+    assertThat(contextCaptor.getValue().getBlockHeader()).isEqualTo(block.getHeader());
+    assertThat(contextCaptor.getValue().getProtocolSpec()).isSameAs(protocolSpec);
+    assertThat(contextCaptor.getValue().getBlockHashLookup()).isSameAs(blockHashLookup);
+    assertThat(contextCaptor.getValue().getWorldState()).isSameAs(worldState);
+    assertThat(contextCaptor.getValue().getOperationTracer()).isSameAs(OperationTracer.NO_TRACING);
+    assertThat(contextCaptor.getValue().getBlockAccessListBuilder()).isEmpty();
   }
 }
