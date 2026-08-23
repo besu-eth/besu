d568aacc2d525e5fba2aa76972a06650a2596ea5
Sally MacFarlane <macfarla.github@gmail.com>
2026-08-24 08:18:52 +1000

    Add missing assertNotNull import
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/mainnet/parallelization/OptimisticTransactionProcessorUnitTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/mainnet/parallelization/OptimisticTransactionProcessorUnitTest.java
@@ -16,6 +16,7 @@ package org.hyperledger.besu.ethereum.mainnet.parallelization;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hyperledger.besu.ethereum.trie.pathbased.common.worldview.WorldStateConfig.createStatefulConfigWithTrie;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
